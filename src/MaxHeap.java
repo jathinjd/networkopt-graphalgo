87323861fb62ff824c4bb069d2d2ef11569b7d10
Jathin Dhulipalla <jathin57@gmail.com>
2020-05-24 05:33:34 -0500

    Add Heap class and functionality

--- src/MaxHeap.java
+++ src/MaxHeap.java
@@ -1,3 +1,128 @@
-public class MaxHeap {
+import java.util.Arrays;
 
+public class MaxHeap {
+    private int size;
+    private int capacity;
+    private int[] elements;
+
+    public MaxHeap(){
+        size = 0;
+        capacity = 10;
+        elements = new int[capacity];
+    }
+
+    public MaxHeap(int capacity){
+        this.capacity = capacity;
+        size = 0;
+        elements = new int[this.capacity];
+    }
+
+    public int size(){
+        return size;
+    }
+
+    private int getLeftChildIndex(int index){
+        return ((index * 2) + 1);
+    }
+    private int getRightChildIndex(int index){
+        return ((index * 2) + 2);
+    }
+    private int getParentIndex(int index){
+        return ((index - 1) / 2);
+    }
+
+    private int getLeftChild(int index){
+        return elements[getLeftChildIndex(index)];
+    }
+    private int getRightChild(int index){
+        return elements[getRightChildIndex(index)];
+    }
+    private int getParent(int index){
+        return elements[getParentIndex(index)];
+    }
+
+    private boolean hasLeftChild(int index){
+        return getLeftChildIndex(index) < size;
+    }
+    private boolean hasRightChild(int index){
+        return getRightChildIndex(index) < size;
+    }
+    private boolean hasParent(int index){
+        return getParentIndex(index) >= 0;
+    }
+
+    private void ensureCapacity(){
+        if(size == capacity){
+            capacity *= 2;
+            elements = Arrays.copyOf(elements, capacity);
+        }
+    }
+
+    private void swapElements(int index1, int index2){
+        int temp = elements[index1];
+        elements[index1] = elements[index2];
+        elements[index2] = temp;
+    }
+
+    public int maximum(){
+        return elements[0];
+    }
+
+    public int poll(){
+        int max = maximum();
+        delete();
+        return max;
+    }
+
+    public void insert(int element){
+        ++size;
+        ensureCapacity();
+        elements[size - 1] = element;
+        maxHeapifyUp();
+    }
+
+    public void delete(){
+        swapElements(0, size - 1);
+        elements[--size] = 0;
+        maxHeapifyDown();
+    }
+
+    private void maxHeapifyUp(){
+        int index = size - 1;
+        while(hasParent(index) && getParent(index) < elements[index]){
+            swapElements(index, getParentIndex(index));
+            index = getParentIndex(index);
+        }
+    }
+
+    private void maxHeapifyDown(){
+        int index = 0;
+        while(hasLeftChild(index)){
+            int swapIndex = getLeftChildIndex(index);
+            if(hasRightChild(index) && getRightChild(index) > getLeftChildIndex(index)){
+                swapIndex = getRightChildIndex(index);
+            }
+            if(elements[index] > elements[swapIndex]) break;
+            else swapElements(index, swapIndex);
+            index = swapIndex;
+        }
+    }
+
+    public static void main(String args[]){
+        MaxHeap mhp = new MaxHeap();
+        mhp.insert(1);
+        mhp.insert(2);
+        mhp.insert(3);
+        mhp.insert(4);
+        mhp.insert(5);
+        mhp.insert(6);
+        System.out.println(mhp.size());
+        System.out.println(mhp.poll());
+        System.out.println(mhp.size());
+        System.out.println(mhp.maximum());
+        System.out.println(mhp.size());
+        mhp.insert(100);
+        System.out.println(mhp.maximum());
+        System.out.println(mhp.size());
+    }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,27 +1,5 @@
 public class Test {
 
     public static void main(String[] args){
-        int[][] A = new int[4][2];
-        A[0][0] = 0;
-        A[0][1] = 2;
-        A[1][0] = 5;
-        A[1][1] = 10;
-        A[2][0] = 13;
-        A[2][1] = 23;
-        A[3][0] = 24;
-        A[3][1] = 25;
-
-        int[] s1 = new int[A.length * 2];
-
-        for(int i = 0; i < A.length; ++i){
-            for(int j = 0; j < 2; ++j){
-                s1[i + j] = A[i][j];
-                System.out.print(s1[i+j] + " ");
-            }
-        }
-        for(int i = 0; i < s1.length; ++i){
-            System.out.println("i" + i + " " + s1[i]);
-        }
-
     }
 }
